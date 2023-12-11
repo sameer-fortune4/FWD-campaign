f7c3ec8e04b2160b6a5e39f4c06c1aeb91d431eb
vikas God <vikas.g@fortune4.in>
2023-12-11 15:02:40 +0530

    add login

--- component/landing/home.js
+++ component/landing/home.js
@@ -3,52 +3,70 @@ import { useEffect } from 'react';
 import homeStyle from "../../styles/home.module.scss";
 import commonStyle from "../../styles/Common.module.css"
 import lottie from 'lottie-web';
+import { useRouter } from 'next/router';
+import { signIn, useSession } from 'next-auth/react';
 export default function Home() {
-    const[open, setOpen] = useState(false)
-    const handleClick = () =>{
+
+    const { data: session } = useSession();
+    const router = useRouter()
+
+    const [open, setOpen] = useState(false)
+    const handleClick = () => {
         setOpen(true)
     }
+
     useEffect(() => {
         // Load the Lottie animation
         const animation = lottie.loadAnimation({
-          container: document.getElementById('bg-wrapper'),
-          renderer: 'svg',
-          loop: true,
-          autoplay: true,
-          path: '/assets/js/lottiefiles.json',
+            container: document.getElementById('bg-wrapper'),
+            renderer: 'svg',
+            loop: true,
+            autoplay: true,
+            path: '/assets/js/lottiefiles.json',
         });
-    
+
         // Clean up the animation when the component is unmounted
         return () => {
-          animation.destroy();
+            animation.destroy();
         };
-      }, []);
-  return (
-    <>
-        <div id="bg-wrapper" className={commonStyle["bg-animation"]}></div>
-        <section className={homeStyle["banner-wrapper"]} >
-            <h1 className={commonStyle["large-title"]}>PLAYLIST FOR A PROBLEM</h1>
-            <p className={homeStyle["tag-lines"]}>Sharing your feelings is healthy. Tell us how you feel today and we'll curate an album
-                designed to make you feel better.</p>
-            <div className={homeStyle["form-group"]}>
-                <input type="text"
-                    placeholder="I'm so anxious that I might lose my job I'm worried it's driving my wife away."/>
-            </div>
-            <a href="#" className={homeStyle["btn"]} onClick={()=> setOpen(true)}>Generate</a>
-        </section>
-        {
-            open == true ? <>
-                <div className={homeStyle["dialog-wrapper"]}>
-                    <div className={homeStyle["dialog-box"]}>
-                        <h4 className={homeStyle["modal-title"]}>Log in with your spotify account to get the most personalised experience.</h4>
-                        <div className={homeStyle["btn-wrap"]}>
-                            <a href="#" className={homeStyle["btn"] + " " + homeStyle["confirm-btn"]}>Okay</a>
-                            <a href="#" className={homeStyle["btn"] + " " + homeStyle["cancel-btn"]} onClick={()=> setOpen(false)}>Cancel</a>
+    }, []);
+
+    const login = () => {
+        signIn('spotify')
+    }
+
+    useEffect(() => {
+        if (session) {
+            router.push("/play-list")
+        }
+    }, [session])
+
+    return (
+        <>
+            <div id="bg-wrapper" className={commonStyle["bg-animation"]}></div>
+            <section className={homeStyle["banner-wrapper"]} >
+                <h1 className={commonStyle["large-title"]}>PLAYLIST FOR A PROBLEM</h1>
+                <p className={homeStyle["tag-lines"]}>Sharing your feelings is healthy. Tell us how you feel today and we'll curate an album
+                    designed to make you feel better.</p>
+                <div className={homeStyle["form-group"]}>
+                    <input type="text"
+                        placeholder="I'm so anxious that I might lose my job I'm worried it's driving my wife away." />
+                </div>
+                <a href="#" className={homeStyle["btn"]} onClick={() => setOpen(true)}>Generate</a>
+            </section>
+            {
+                open == true ? <>
+                    <div className={homeStyle["dialog-wrapper"]}>
+                        <div className={homeStyle["dialog-box"]}>
+                            <h4 className={homeStyle["modal-title"]}>Log in with your spotify account to get the most personalised experience.</h4>
+                            <div className={homeStyle["btn-wrap"]}>
+                                <a href="#" className={homeStyle["btn"] + " " + homeStyle["confirm-btn"]} onClick={login}>Okay</a>
+                                <a href="#" className={homeStyle["btn"] + " " + homeStyle["cancel-btn"]} onClick={() => setOpen(false)}>Cancel</a>
+                            </div>
                         </div>
                     </div>
-                </div>
                 </> : <></>
-        }
-    </>
-  )
+            }
+        </>
+    )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,10 @@
-import { signIn, useSession } from 'next-auth/react'
-import { useRouter } from 'next/router';
-import React, { useEffect } from 'react'
+import React from 'react'
 import LandingWrapper from '../component/landing/landingWrapper';
 
 export default function Index() {
 
-    // const SPORTFY_AUTHENTICATION = "https://accounts.spotify.com/authorize"
-    // const SPOTIFY_CLIENT_ID = "e5e4c494c00742d8845eab9e97cf26a2"
-    // const redirect_url = "http://localhost:3000/play-list"
-    // const space = "%20"
-    // const scope = ["user-read-currently-playing", "user-top-read"];
-    // const scope_url_param = scope.join(space)
-
-    // const handleLogin = () => {
-    //     window.location = `${SPORTFY_AUTHENTICATION}?client_id=${SPOTIFY_CLIENT_ID}&response_type=code&redirect_uri=${redirect_url}&scope=${scope_url_param}&show_dialog=true`
-    // }
-    const login = () => {
-        signIn('spotify')
-    }
-
-    const { data: session } = useSession();
-    const router = useRouter()
-
-    useEffect(() => {
-        if (session) {
-            router.push("/play-list")
-        }
-    }, [session])
     return (
         <>
-            {/* <button className='loginButton' onClick={login} >
-                login
-            </button> */}
             <LandingWrapper/>
         </>
     )
