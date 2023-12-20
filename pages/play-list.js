56b311f831b24b720778ae9285fe72c2385a0794
vikas God <vikas.g@fortune4.in>
2023-12-20 14:58:13 +0530

    changes

--- pages/play-list.js
+++ pages/play-list.js
@@ -14,12 +14,10 @@ import { Navigation, EffectCoverflow } from 'swiper/modules';
 import lottie from 'lottie-web';
 import Chatbox from '../component/common/chatbox';
 import LoaderCard from '../component/common/loaderCard';
-import dynamic from 'next/dynamic';
 // import Loader from '../component/landing/loader';
-// const lottie = dynamic(()=>import('lottie-web'))
+
 export default function PlayList() {
     const { data: session } = useSession();
-    const router = useRouter();
 
     const [listSong, setListSong] = useState([]);
     const [idData, setIdData] = useState()
@@ -37,11 +35,9 @@ export default function PlayList() {
         // }
         if (!session) {
             if (token) {
-
                 fetchFilterListSong(token)
             }
         } else {
-
             fetchFilterListSong(access_token)
         }
     }, [session]);
@@ -64,53 +60,19 @@ export default function PlayList() {
     const [isLoading, setIsloading] = useState(false)
     const fetchFilterListSong = async (access_token) => {
         setIsloading(true)
-        let item = JSON.parse(localStorage.getItem('emotionData'))
-        let response = await getData(apiList.FILTER_BY_TRACK_SONG + `?q=${item?.name}&access_token=${access_token}`)
+        // let item = JSON.parse(localStorage.getItem('emotionData'))
+        let item = localStorage.getItem('inputData')
+        // console.log("object");
+        let response = await getData('api/filter/filterByTrackSong' + `?q=${item}&access_token=${access_token}`)
         if (response) {
             setListSong(response.items)
             setIsloading(false)
         }
     }
 
-    const [audioRef, setAudioRef] = useState(null);
     const [playSong, setPlaySong] = useState()
-    const [currentTime, setCurrentTime] = useState(0);
-    const [duration, setDuration] = useState(0);
-    useEffect(() => {
-        // Create an audio element reference
-        const audioElement = new Audio();
-        setAudioRef(audioElement);
 
-        const handleTimeUpdate = () => {
-            setCurrentTime(audioElement.currentTime);
-        };
-
-        // Handle duration change
-        const handleDurationChange = () => {
-            setDuration(audioElement.duration);
-        };
-
-        // Attach event listeners
-        audioElement.addEventListener('timeupdate', handleTimeUpdate);
-        audioElement.addEventListener('durationchange', handleDurationChange);
-
-        // Clean up the audio element when the component is unmounted
-        return () => {
-            audioElement.pause();
-            audioElement.src = '';
-            audioElement.load();
-            audioElement.removeEventListener('timeupdate', handleTimeUpdate);
-            audioElement.removeEventListener('durationchange', handleDurationChange);
-        };
-    }, []);
     const handledata = async (data) => {
-        console.log("object", playSong);
-        // Pause the previous song
-        if (audioRef) {
-            audioRef.pause();
-        }
-
-        // If the clicked card is the same as the currently playing one, pause the audio
         if (idData === data) {
             setIdData(null);
         } else {
@@ -121,26 +83,14 @@ export default function PlayList() {
             try {
                 // let access_token = session?.accessToken;
                 let access_token = localStorage.getItem('access_token');
-                let response = await getData(apiList.SINGLE_SONG + `?access_token=${access_token}&id=${data}`);
+                let response = await getData('api/filter/track' + `?access_token=${access_token}&id=${data}`);
                 setPlaySong(response.preview_url);
-
-                // Play the audio
-                if (audioRef) {
-                    audioRef.src = response.preview_url;
-                    audioRef.load();
-                    audioRef.play();
-                }
             } catch (error) {
                 console.error('Error fetching playlist song data:', error);
             }
         }
     };
-    const calculateProgressBarWidth = () => {
-        if (duration > 0) {
-            return (currentTime / duration) * 100 + '%';
-        }
-        return '0%';
-    };
+
     const swiperOptions = {
         className: "mySwiper",
         effect: 'coverflow',
@@ -174,52 +124,10 @@ export default function PlayList() {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
-        // on: {
-        //     slideChange: (swiper) => {
-        //         setClassData(false);
-        //         if (audioRef) {
-        //             audioRef.pause();
-        //         }
-        //     },
-        // },
     }
-
-    const [swiper, setSwiper] = useState(null);
-    // useEffect(() => {
-    //     if (swiper) {
-    //         swiper.on('slideChange', () => {
-    //             setClassData(false);
-    //             if (audioRef) {
-    //                 audioRef.pause();
-    //             }
-    //         });
-    //     }
-    // }, [swiper]);
-
-    const handleSwiper = (swiperInstance) => {
-        setSwiper(swiperInstance);
-
-        swiperInstance.on('slideChange', () => {
-            setClassData(false);
-        });
-    };
-
-    useEffect(() => {
-        if (audioRef && classData === false) {
-            audioRef.pause()
-        }
-    }, [classData])
-    // console.log("object", audioRef);
-
-    const formatTime = (timeInSeconds) => {
-        const minutes = Math.floor(timeInSeconds / 60);
-        const seconds = Math.floor(timeInSeconds % 60);
-        return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
-    };
     return (
         <>
-            {/* <div id="bg" className={commonStyle["bg-animation"]} ></div> */}
-            <div className={commonStyle["bg-gradient"]}></div>
+            <div id="bg" className={commonStyle["bg-animation"]} ></div>
             <div className={styles["swiper-box"]}>
                 <div className={styles.container + " " + "swiper-container"}>
                     <h2 className={styles["medium-title"]}>Listen to other people's</h2>
@@ -227,8 +135,7 @@ export default function PlayList() {
                     {isLoading ?
                         <LoaderCard />
                         :
-                        <>
-                        <Swiper {...swiperOptions} onSwiper={handleSwiper}>
+                        <Swiper {...swiperOptions}>
                             {listSong &&
                                 listSong.map((v, i) => (
                                     <SwiperSlide key={i}>
@@ -236,50 +143,22 @@ export default function PlayList() {
                                             <div className={styles.image}>
                                                 <img src={v.album?.images[0]?.url} alt={v.name} />
                                                 <div className={styles["player"] + " " + "player-wrapper"}>
-                                                    <span className={styles["media-icon"] + " " + ((idData === v.id && classData ? styles.active : ""))}></span>
-                                                    <div className={styles["control-wrapper"]}>
-                                                        <span className={styles["info-title"]}>{v.name}</span>
-                                                        <div className={styles['timer-wrap']}>
-                                                            {idData === v.id && classData ? <>
-                                                                <span>{formatTime(currentTime)}</span>
-                                                                <span>{formatTime(duration)}</span>
-                                                            </>
-                                                                :
-                                                                <>
-                                                                    <span>0.00</span>
-                                                                    <span>{formatTime(duration)}</span>
-                                                                </>
-                                                            }
-                                                        </div>
-                                                        <source ref={audioRef} src={playSong} type="audio/mpeg" />
-                                                        <div className={styles["progress-bar"]}>
-                                                            {idData === v.id && classData ?
-                                                                <span
-                                                                    className={styles["strip"]}
-                                                                    style={{ width: calculateProgressBarWidth() }}
-                                                                ></span>
-                                                                :
-                                                                <span
-                                                                    className={styles["strip"]}
-                                                                ></span>
-                                                            }
-                                                        </div>
-
-                                                        {/* <div className={styles["progress-bar"]}>
-                                                                <span className={styles["strip"]}></span>
-                                                            </div> */}
-                                                    </div>
+                                                    {idData == v.id && playSong !==undefined &&
+                                                        <audio controls>
+                                                            <source src={playSong} type="audio/mpeg" />
+                                                        </audio>
+                                                    }
+                                                    
                                                 </div>
                                                 <span className={styles["btn-play"] + " " + "play-btn"}></span>
                                             </div>
                                         </a>
                                     </SwiperSlide>
                                 ))}
+
                             <div className="swiper-button-prev swipe-btn"></div>
                             <div className="swiper-button-next swipe-btn"></div>
                         </Swiper>
-                        <a className={styles["listen-txt"]}>Click to listen</a>
-                        </>
                     }
                 </div>
             </div>
