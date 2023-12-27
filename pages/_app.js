cad063619a0090c8eac925c2088abbcc61530fb7
vikas God <vikas.g@fortune4.in>
2023-12-27 11:11:44 +0530

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from "next/head"
 import "../styles/fonts.css"
 import '../styles/globals.css'
 import { SessionProvider } from "next-auth/react"
@@ -5,6 +6,9 @@ import { SessionProvider } from "next-auth/react"
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>FWD Campaign</title>
+      </Head>
       <SessionProvider session={pageProps.session}>
         <Component {...pageProps} />
       </SessionProvider>
--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,6 @@ export default function Document() {
     return (
         <Html lang='en'>
             <Head>
-                <title>FWD Campaign</title>
                 <meta name="description" content="Your default meta description." />
             </Head>
             <body>
