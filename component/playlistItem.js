4c5010622560b77457942e0304b70682a2b8444b
vikas God <vikas.g@fortune4.in>
2024-01-25 17:35:14 +0530

    changes

--- component/playlistItem.js
+++ component/playlistItem.js
@@ -1,37 +1,35 @@
 import React, { useState } from 'react'
 import styles from '../styles/Common.module.scss';
+import Image from 'next/image';
 
 export default function PlaylistItem({ data, classData, setClassData }) {
-    const [idData, setIdData] = useState()
-    console.log("object",idData);
-    const handledata = async (data) => {
-        setIdData(data);
-        setClassData(true)
+    const [showIframe, setShowIframe] = useState(false)
 
+    const handledata = () => {
+        setShowIframe(true)
     };
+
     return (
-        <>
-            <div className={"swiper-outer"}>
-                <div className={styles.card + " " + (classData == true ? "control" : "")} onClick={() => handledata(data.id)} >
-                    <div className={styles.image + " " + "sliderCard-img"} >
-                        <img width={200} height={200} src={data.album?.images[0]?.url} alt={data.name} />
-                        <div className={styles["player"] + " " + "player-wrapper"}>
+        <div className={"swiper-outer"}>
+            <div className={styles.card + " " + (classData == true ? "control" : "")}  >
+                <div className={styles.image + " sliderCard-img"} >
+                    {showIframe ?
+                        <div className={styles["player"] + " player-wrapper"}>
                             <div className={styles["wrap-control"]}>
-                                {data.id === idData &&
-                                    <iframe
-                                    src={`https://open.spotify.com/embed/track/${idData}?utm_source=generator`}
-                                    // width="300"
-                                    height="200"
+                                <iframe
+                                    src={`https://open.spotify.com/embed/track/${data.id}?utm_source=generator`}
+                                    width="300"
+                                    height="360"
                                     frameBorder="0"
                                     allowtransparency="true"
                                     allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
                                 ></iframe>
-                                }
                             </div>
-                        </div>
-                    </div>
+                        </div> :
+                        <Image width={200} height={200} src={data.album?.images[0]?.url} alt={data.name} onClick={() => handledata()} />
+                    }
                 </div>
             </div>
-        </>
+        </div>
     )
 }
